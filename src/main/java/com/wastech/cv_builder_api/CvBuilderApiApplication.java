99a5daa16d6e1704db0e6acbb72544b6d83aea93
wastech <fataiwasiu2@gmail.com>
2024-10-29 15:18:02 +0100

    setup :zap:

---
+++ src/main/java/com/wastech/cv_builder_api/CvBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.wastech.cv_builder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvBuilderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wastech/cv_builder_api/CvBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wastech.cv_builder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
